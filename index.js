843bac92c9c86f6cbc8ed13966b591be10886fd0
Abdullah149081 <masudmd425@gmail.com>
2023-05-13 11:19:22 +0600

    banner and services api

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const banner = require("./data/banner.json");
-const services = require("./data/services.json");
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/banner", (req, res) => {
-  res.send(banner);
-});
-
-app.get("/services", (req, res) => {
-  res.send(services);
-});
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ignmh8y.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,15 +24,26 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const database = client.db("carDoctorDB");
-    const carCollection = database.collection("carDoctor");
+    const servicesCollection = client.db("carDoctorDB").collection("services");
+    const bannerCollection = client.db("carDoctorDB").collection("banner");
+
+    app.get("/banner", async (req, res) => {
+      const banner = bannerCollection.find();
+      const result = await banner.toArray();
+      res.send(result);
+    });
+
+    app.get("/services", async (req, res) => {
+      const result = await servicesCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
